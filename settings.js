d838b7ae4ee81bb1dbbf4571ed6926af4f1fa717
professorktk-cell <professorktk@gmail.com>
2026-06-12 09:09:26 +0500

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,6 @@
+module.exports = {
+    giphyApiKey: process.env.GIPHY_API_KEY || 'dc6zaTOxFJmzC',
+    ownerNumber: process.env.OWNER_NUMBER || '923000000000',
+    botName: 'OLD-STUDIO MD',
+    ownerName: 'OLD-STUDIO'
+};
