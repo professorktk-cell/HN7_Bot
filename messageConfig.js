d4cfc05984205faa3df1326fa27c7ca6334a9fe6
professorktk-cell <professorktk@gmail.com>
2026-06-12 09:25:06 +0500

    Add files via upload

---
+++ messageConfig.js
@@ -0,0 +1,13 @@
+module.exports = {
+    channelInfo: {
+        contextInfo: {
+            forwardingScore: 999,
+            isForwarded: true,
+            forwardedNewsletterMessageInfo: {
+                newsletterJid: '120363348739987203@newsletter',
+                newsletterName: 'OLD-STUDIO MD',
+                serverMessageId: -1
+            }
+        }
+    }
+};
